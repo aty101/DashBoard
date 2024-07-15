98ae571308be5386968976a614af43b27c7b052f
aty101 <atyaty003@gmail.com>
2024-07-15 03:42:24 +0300

    part of the dashBoard

--- src/dashBoard/DashBoard.jsx
+++ src/dashBoard/DashBoard.jsx
@@ -2,11 +2,18 @@ import SideBar from "./SideBar";
 import { FaSearch } from "react-icons/fa";
 import "./DashBoard.css";
 import "bootstrap/dist/css/bootstrap.css";
+var requests = [
+  {
+    name: "aty",
+    city: "القاهرة",
+    collage: "حاسبات",
+  },
+];
 
 function DashBoard() {
   return (
     <div className="requestPage d-flex flex-row">
-      <SideBar ></SideBar>
+      <SideBar></SideBar>
 
       <div className="d-flex flex-column justify-content-center mb-2 mt-2 body me-4">
         <div className="navbar navbar-expand-lg bg-body-tertiary filters me-5">
@@ -38,19 +45,26 @@ function DashBoard() {
                 <th scope="col">#</th>
                 <th scope="col">اسم الطالب</th>
                 <th scope="col">المحافظة</th>
-                <th scope="col">الك</th>
+                <th scope="col">الكلية</th>
 
                 <th scope="col">Action</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>logo</td>
-
-                <td className="text-center "></td>
-              </tr>
+              {requests.map((item, key) => {
+                return (
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>{item.name}</td>
+                    <td>{item.city}</td>
+                    <td>{item.collage}</td>
+                    <td className="text-center">
+                      {" "}
+                      <div></div>
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
